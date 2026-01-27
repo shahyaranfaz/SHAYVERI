1f9a7e762568f340b44362ff48247a1589f28a44
shahyaranfaz <shahyar.anfaz@mail.utoronto.ca>
2026-01-26 20:03:36 -0500

    Add comments for opening book functionality
    Add comments to clarify the purpose of the opening book.

--- opening.py
+++ opening.py
@@ -1,3 +1,6 @@
 import pandas as pd
 
 opening_book = pd.read_csv("table.csv")
+
+# We will read from the opening book and choose the best move. we can decide later
+# how to build the best book. Once the book lookup fails, we transition to midgame.py
